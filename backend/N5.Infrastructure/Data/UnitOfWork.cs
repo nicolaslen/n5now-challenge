3d151642606bb869aa2bc766a9309094e7837992
Nicolás Len <nicolaslen@gmail.com>
2023-08-24 02:50:00 -0300

    .

--- backend/N5.Infrastructure/Data/UnitOfWork.cs
+++ backend/N5.Infrastructure/Data/UnitOfWork.cs
@@ -6,10 +6,6 @@ using N5.Core.DTOs;
 
 namespace N5.Infrastructure.Data
 {
-    //https://github.com/matthewrenze/clean-architecture-demo/blob/repo-and-uow/Application/Application.csproj OJO .NET (no net core)
-    //https://learn.microsoft.com/en-us/aspnet/mvc/overview/older-versions/getting-started-with-ef-5-using-mvc-4/implementing-the-repository-and-unit-of-work-patterns-in-an-asp-net-mvc-application
-    //https://medium.com/@mxcmxc/unit-of-work-with-repository-pattern-boilerplate-518726e4beb7
-
     public class UnitOfWork : IUnitOfWork
     {
         private readonly ApplicationDbContext _dbContext;
